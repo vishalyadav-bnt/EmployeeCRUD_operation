cfefc1101cb24ba16bd11b3e08d30e4b3069ac5d
Vishal Yadav <vishal.yadav@bnt.local>
2024-11-14 11:26:58 +0530

    changes commited for the git action

--- employee/src/test/java/com/example/employee/SonarQubeIntegrationTest.java
+++ employee/src/test/java/com/example/employee/SonarQubeIntegrationTest.java
@@ -11,11 +11,11 @@ public class SonarQubeIntegrationTest {
                 .withExposedPorts(9000)
                 .waitingFor(Wait.forHttp("/").forStatusCode(200).withStartupTimeout(Duration.ofMinutes(5)))) {
 
-            // Start the SonarQube container
+          
             sonarqube.start();
             System.out.println("SonarQube started: " + sonarqube.isRunning());
 
-            // Get and print the mapped port to confirm it started correctly
+           
             Integer mappedPort = sonarqube.getMappedPort(9000);
             System.out.println("SonarQube is mapped to localhost port: " + mappedPort);
 
@@ -26,7 +26,7 @@ public class SonarQubeIntegrationTest {
                 System.err.println("SonarQube container failed to start.");
             }
 
-            // Add more code here to trigger SonarQube analysis if needed
+            
         } catch (Exception e) {
             System.err.println("Error starting SonarQube container: " + e.getMessage());
             e.printStackTrace();
